1274636b62ab98841e6d962b6c8dfbaba16579a2
Gert2510 <gert@seebacher-experten.com>
2024-12-06 09:55:33 +0100

    feat: password as hash
    Signed-off-by: Gert2510 <gert@seebacher-experten.com>

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '1eb02309b7cefc0514bb374f'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
