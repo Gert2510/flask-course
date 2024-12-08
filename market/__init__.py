ae4abe8588654da06decde35187a99bebf5a20aa
Gert2510 <gert@seebacher-experten.com>
2024-12-08 16:41:15 +0100

    chore: login (not working - mehthod not allowed)

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '1eb02309b7cefc0514bb374f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-
+from flask_login import login_user
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -33,4 +33,16 @@ def register_page():
 @app.route('/login')
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.date).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Successs! You have been logged in as {attempted_user.username} ', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and pasword incorrect!', category='danger')
+
+
     return render_template('login.html', form=form)
