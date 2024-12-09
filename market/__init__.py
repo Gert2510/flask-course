1e922839ec97abd035e237f3b2ce2c90aa561abd
Gert2510 <gert@seebacher-experten.com>
2024-12-09 09:51:42 +0100

    chore: logout & customizations

--- market/__init__.py
+++ market/__init__.py
@@ -8,5 +8,6 @@ app.config['SECRET_KEY'] = '1eb02309b7cefc0514bb374f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 from market import routes
